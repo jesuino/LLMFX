ed2f91e0e66a48011a7fddaf188aa9794432cae4
William Siqueira <william.fatecsjc@gmail.com>
2025-04-18 14:47:52 -0300

    Adding a command tool

--- src/main/java/org/fxapps/llmfx/tools/ToolsInfo.java
+++ src/main/java/org/fxapps/llmfx/tools/ToolsInfo.java
@@ -3,6 +3,7 @@ package org.fxapps.llmfx.tools;
 import java.util.List;
 import java.util.Map;
 
+import org.fxapps.llmfx.tools.code.CommandsTool;
 import org.fxapps.llmfx.tools.files.FilesReaderTool;
 import org.fxapps.llmfx.tools.files.FilesWriterTool;
 import org.fxapps.llmfx.tools.time.DateTimeTool;
@@ -17,17 +18,20 @@ public class ToolsInfo {
     private static final String FILES_READ = "Files Read";
     private static final String WEB_SEARCH = "Web Search";
     private static final String DATE_TIME = "Date and Time";
+    private static final String COMMANDS = "Commands";
 
     Map<String, Object> toolsMap = Map.of(
             FILES_READ, new FilesReaderTool(),
             FILE_WRITE, new FilesWriterTool(),
             WEB_SEARCH, new WebSearchTool(),
-            DATE_TIME, new DateTimeTool());
+            DATE_TIME, new DateTimeTool(),
+            COMMANDS, new CommandsTool());
 
     Map<String, List<String>> toolsCategoryMap = Map.of(
             "Files", List.of(FILES_READ, FILE_WRITE),
-            "Web", List.of(WEB_SEARCH), 
-            "Date and Time", List.of(DATE_TIME));
+            "Web", List.of(WEB_SEARCH),
+            "Date and Time", List.of(DATE_TIME),
+            "Execute", List.of(COMMANDS));
 
     public Map<String, List<String>> getToolsCategoryMap() {
         return toolsCategoryMap;
---
+++ src/main/java/org/fxapps/llmfx/tools/code/CommandsTool.java
@@ -0,0 +1,42 @@
+package org.fxapps.llmfx.tools.code;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import dev.langchain4j.agent.tool.P;
+import dev.langchain4j.agent.tool.Tool;
+
+public class CommandsTool {
+
+    @Tool("""
+            Run a command in the terminal and return the output.
+            """)
+    public String runCommand(@P("The command to run and its arguments") List<String> command) throws Exception {
+
+        var process = new ProcessBuilder(command).start();
+        // Reading the output from the command
+        var reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        var result = new StringBuffer();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line).append(System.lineSeparator());
+        }
+        reader.close();
+
+        // Waiting for the command to complete and checking its exit value
+        int exitCode = process.waitFor();
+
+        if (exitCode != 0) {
+            throw new RuntimeException("Command failed with exit code: " + exitCode);
+        }
+        return result.toString();
+
+    }
+
+    @Tool("Get the name of the operating system")
+    public String osName() {
+        return System.getProperty("os.name");
+    }
+
+}
